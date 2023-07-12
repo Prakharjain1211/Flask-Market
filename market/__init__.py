cd9fa47de5a88aa65309673012d490f4ad0dbfdb
Prakharjain1211 <88723250+Prakharjain1211@users.noreply.github.com>
2023-07-12 06:56:48 +0000

    Flask Validations

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '9c8d96d7f41b5327904f5bb8'
 db = SQLAlchemy(app)
 app.app_context().push()
+
 from market import routes
